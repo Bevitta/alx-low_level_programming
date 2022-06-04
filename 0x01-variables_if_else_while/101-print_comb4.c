f4d506abc252b7e3e335ccbf477e0ad0997c2338
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-04 00:02:02 -0700

    atask11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 	int b;
 	int c;
 
-	for (a = 0; a <= 8; a++)
+	for (a = 0; a < 8; a++)
 	{
-		for (b = a + 1; b <= 9; b++)
+		for (b = a + 1; b < 9; b++)
 		{
-			for (c = b + 1; c <= 10; c++)
+			for (c = b + 1; c < 10; c++)
 			{
 				putchar(a + '0');
 				putchar(b + '0');
