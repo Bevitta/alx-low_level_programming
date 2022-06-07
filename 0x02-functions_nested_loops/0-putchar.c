1c01b63cc21965f5f25e9ddd9db9107048cf61a4
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-07 04:55:49 -0700

    thattask0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,14 +9,14 @@
  */
 int main(void)
 {
-	char a[] = "_putchar";
-	int i = 0;
-
-		for (i = 0; i < 8; i++)
-		{
-			_putchar(a[i]);
-		}
-	_putchar('\n');
-
-	return (0);
+_putchar(95);
+_putchar(112);
+_putchar(117);
+_putchar(116);
+_putchar(99);
+_putchar(104);
+_putchar(97);
+_putchar(114);
+_putchar('\n');
+return (0);
 }
