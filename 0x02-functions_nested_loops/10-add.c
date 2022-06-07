7eaaa3810fad24844aabc851b6c8a77be1d1cf22
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-07 11:18:51 -0700

    task10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *add - will add two numbers
+ *@x: first number
+ *@y: second number
+ *
+ *Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
