3365d3df747f49a3fdec8dcc2641b9081289015d
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-07 00:58:23 -0700

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *main - prints _putchar
+ *
+ *Description: prints _putchar by invoking it
+ *
+ *Return: 0 (success)
+ */
+int main(void)
+{
+	char c[8] = "_putchar"
+	int a = 0;
+
+		for (a = 0; a < 8; a++)
+		{
+			_putchar(c[a]);
+		}
+	_putchar("\n');
+
+	return (0);
+}
