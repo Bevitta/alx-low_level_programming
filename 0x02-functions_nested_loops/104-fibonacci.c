55bf0c84b678dae0c90f8070de78948bbedd3652
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-08 04:10:40 -0700

    ctask16

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -3,10 +3,22 @@
 /**
  * main - finds and prints the first 98 Fibonacci numbers,
  * starting with 1 and 2
- * 
+ *
  * Return: ALways 0 (Success)
  */
 int main(void)
 {
+	int i;
+	int a = 0, b = 1;
+	int nextTerm = t1 + t2;
 
+	printf("%d, %d, ", a, b);
+	for (i = 3; i <= 98; ++i)
+	{
+		printf("%d, ", nextTerm);
+		a = b;
+		b = nextTerm;
+		nextTerm = a + b;
+	}
+	return (0);
 }
