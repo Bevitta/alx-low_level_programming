05771c70233a259e9403be28c42db2a2967b8259
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-09 07:34:46 -0700

    task11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main - prints largest prime factor of given number
+*
+* Return: 0 (success)
+*/
+int main(void)
+{
+	long i, number = 612852475143;
+
+	for (i  = 2; i <= number; i++)
+	{
+		if (number % i == 0)
+		{
+			number = number / i;
+			i--;
+		}
+	}
+	printf("%lu\n", i);
+	return (0);
+}
