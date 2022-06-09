64f7b6a797ae44f6b89cdc122ff8a8fe30895faa
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-09 04:55:49 -0700

    task5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,19 +3,19 @@
 /**
  * more_numbers - prints numbers between 0 to 14
  * 10 times.
- * Return: no return.
+ *
  */
 void more_numbers(void)
 {
-	int i, ch;
+	int i, c;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (ch = 0; ch < 15; ch++)
+		for (c = 0; c < 15; c++)
 		{
-			if (ch >= 10)
-				_putchar((ch / 10) + 48);
-			_putchar((ch % 10) + 48);
+			if (c >= 10)
+				_putchar((c / 10) + 48);
+			_putchar((c % 10) + 48);
 		}
 		_putchar('\n');
 	}
