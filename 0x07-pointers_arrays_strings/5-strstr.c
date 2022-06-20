095e4fe7709d9cc00ca53eec860eb680c49b992e
Bevitta <105193293+Bevitta@users.noreply.github.com>
2022-06-20 07:49:58 -0700

    task7

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strstr - locates a substring in a string
+* @haystack: substring to be located
+* @needle: the given string
+* Return: pointer to located substring or NULL if substring not found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
